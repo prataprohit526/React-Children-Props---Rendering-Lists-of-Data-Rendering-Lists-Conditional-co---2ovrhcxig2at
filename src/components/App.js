70cb2ed3a44180a9261b2c0b56f123d110066b6e
prataprohit526 <prataprohit526@gmail.com>
2023-02-20 15:09:38 +0530

    all change

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,19 @@ import React , {useState,useEffect} from 'react'
 import '../styles/App.css';
 import ChildComponent from './ChildComponent';
 const App = () => {
- //code here
+  const [currentNum, setCurrentNum] = useState(0);
+    const handleChange = (e) => {
+        setCurrentNum(Number(e.target.value));
+    };
+    return (
+        <div id="main">
+            <ChildComponent>
+                <input id="input" onChange={(e) => handleChange(e)} />
+            </ChildComponent>
 
-  return (
-    <div id="main">
-      <ChildComponent /> 
-       <input id='input' onChange={} />
-    
-      <p id='output'>{}</p>
-    </div>
-  )
+            <p id="output">{Math.pow(currentNum, 2)}</p>
+        </div>
+    );
 }
 
 
--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const ChildComponent = () => {
+const ChildComponent = (props) => {
 //code here
-  return (
-    <div id="child">
-      Find The Square <br/>
-     
-    </div>
-  )
+return (
+  <div id="child">
+      Find The Square <br />
+      {props.children}
+  </div>
+);
 }
 
 
